fda43917eaf47af177329803ea083c6c4bf1d36a
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-02 11:05:24 +0200

    Data Types Variables && Arrays

--- 02_variables.php
+++ 02_variables.php
@@ -19,3 +19,26 @@
 – variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _)
 – Variables are class sensitive ($name and $NAME are two different variables)
 */
+
+$name = 'Brad'; // String
+$age = 40; // Int
+$has_kids = false; // Bool
+$cash_on_hand =  20.75; // Float
+
+// var_dump($cash_on_hand);
+
+// echo $name . ' is ' . $age . ' years old';
+
+// echo "${name} is ${age} years old";
+
+$x = '5' + '5';
+// var_dump($x);
+// echo 10 - 5;
+// echo 5 * 6;
+// echo 10 / 2;
+// echo 10 % 3;
+
+define('HOST', 'localhost');
+define('DB_NAME', 'dev_db');
+
+echo HOST;
