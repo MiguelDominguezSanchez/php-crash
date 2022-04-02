0a28a5da24f2b8c42ce4432ba417ce535f706ac2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-02 11:25:28 +0200

    conditionals

--- 04_conditionals.php
+++ 04_conditionals.php
@@ -1,1 +1,71 @@
-<
+<?php
+/* ---------- Operators ---------- */
+/*
+< Less than
+> Greater than
+<= Less than or equal to
+>= Greater than or equal to
+== Equal to
+=== Identical to
+!= Not equal to
+!== Not identical to
+*/
+
+/* ---------- If Statements ---------- */
+/*
+** If Statements Syntax
+if (condition) {
+  / code to be executed if condition is true
+}
+*/
+
+$age = 17;
+
+// if ($age >= 18) {
+//   echo 'You are old enough to vote';
+// } else {
+//   echo 'Sorry, you are not old enough to vote';
+// };
+
+$t = date("H");
+
+// if ($t < 12) {
+//   echo 'Guten Morgen';
+// } elseif ($t < 17) {
+//   echo 'Guten Nachmittag';
+// } else {
+//   echo 'Guten Abend';
+// };
+
+$posts = [];
+
+// if (!empty($posts)) {
+//   echo $posts[0];
+// } else {
+//   echo 'No Posts';
+// };
+
+// echo !empty($posts) ? $posts[0] : "No Posts";
+
+// $firstPost = !empty($posts) ? $posts[0] : 'No Posts';
+// $firstPost = !empty($posts) ? $posts[0] : null;
+
+// $firstPost = $posts[0] ?? null;
+
+// echo $firstPost;
+
+$favcolor = 'yellow';
+
+switch ($favcolor) {
+  case 'red':
+    echo 'Your favourite color is red';
+    break;
+  case 'blue':
+    echo 'Your favourite color is blue';
+    break;
+  case 'green':
+    echo 'Your favourite color is green';
+    break;
+  default:
+    echo 'Your favourite color is not red, green or blue';
+}
